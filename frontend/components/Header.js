ca829b4e4f3ac43a099a2a07740c09d2187e39d1
Matheus Ferraresi <matheusferraresi@live.com>
2021-08-07 14:04:57 +0100

    adding global styles

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,16 +1,49 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1 rem;
+  }
+`;
+
+const HeaderStyled = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <div className="bar">
-        <Link href="/">In-da-fit</Link>
+        <Logo>
+          <Link href="/">In da fit</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyled>
   );
 }
