4f8a92eb1fff926cf3c5f5a9bca8554c7779fbce
Matheus Ferraresi <matheusferraresi@live.com>
2021-08-17 14:28:55 +0100

    Fixing global styles typo

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -11,14 +11,15 @@ const GlobalStyles = createGlobalStyle`
     }
 
     html {
-        --red: ##FF0000;
+        --red: #FF0000;
         --black: #393939;
         --grey: #3A3A3A;
         --lightGrey: #E1E1E1;
         --offWhite: #EDEDED;
         --maxWidth: 1000px;
-        --bs: '0 12px 24px 0 rgba(0,0,0,0.09)';
+        --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
         box-sizing: border-box;
+        font-size: 62.5%; //10px, better accessibility in case users set different font-size on their browsers
     }
 
     *, *:before, *:after {
