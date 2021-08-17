800cc7e7f8c51042fcba56b8477ba5237a709381
Matheus Ferraresi <matheusferraresi@live.com>
2021-08-17 17:17:10 +0100

    creating custom hook for forms

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+export default function useForm(initialState = {}) {
+  const [inputs, setInputs] = useState(initialState);
+
+  function handleChange(event) {
+    let { value, name, type, files } = event.target;
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      value[0] = files;
+    }
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initialState);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
