5d729c7927aaee5ea125d25997aade625ced6891
Matheus Ferraresi <matheusferraresi@live.com>
2021-08-17 17:16:58 +0100

    adding create product component

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,43 @@
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: 'Nice Shoes',
+    price: 1234,
+    description: 'These are some nice shoes',
+  });
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
+    </form>
+  );
+}
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,3 +1,10 @@
+import { useState } from 'react';
+import CreateProduct from '../components/CreateProduct';
+
 export default function SellPage() {
-  return <p>Sell!</p>;
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
