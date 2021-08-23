0bee71d37eef9c246d867225e399e64985a0be8a
Matheus Ferraresi <matheusferraresi@live.com>
2021-08-23 14:38:46 +0100

    creating add to cart gql mutation

---
+++ backend/mutations/addToCart.ts
@@ -0,0 +1,43 @@
+/* eslint-disable */
+// TODO: fix eslint errors
+import { KeystoneContext } from '@keystone-next/types';
+import { CartItemCreateInput } from '../.keystone/schema-types';
+import { Session } from '../types';
+
+export default async function addToCart(
+  root: any,
+  { productId }: { productId: string },
+  context: KeystoneContext
+): Promise<CartItemCreateInput> {
+  // Query current user to see if they are signed in
+  const sesh = context.session as Session;
+  if (!sesh.itemId) {
+    throw new Error('You must be logged in to do this!');
+  }
+
+  // Query current user cart
+  const allCartItems = await context.lists.CartItem.findMany({
+    where: { user: { id: sesh.itemId }, product: { id: productId } },
+    resolveFields: 'id,quantity',
+  });
+
+  const [existingCartItem] = allCartItems;
+  // See if the current item is in their cart
+  if (existingCartItem) {
+    // If it is, increment by one
+    return await context.lists.CartItem.updateOne({
+      id: existingCartItem.id,
+      data: {
+        quantity: existingCartItem.quantity + 1,
+      },
+    });
+  }
+
+  // If not create a new cart item
+  return await context.lists.CartItem.createOne({
+    data: {
+      product: { connect: { id: productId } },
+      user: { connect: { id: sesh.itemId } },
+    },
+  });
+}
