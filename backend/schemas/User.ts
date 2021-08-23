8dd8851467977f1529a585279ad8b5799ba1b198
Matheus Ferraresi <matheusferraresi@live.com>
2021-08-23 12:41:11 +0100

    creating cart item schema and relationships

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -2,12 +2,20 @@ import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
 
 export const User = list({
-  // access:
-  // ui
+  // TODO: access:
+  // TODO: ui
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
     password: password(),
+    cart: relationship({
+      ref: 'CartItem.user',
+      many: true,
+      ui: {
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
+      },
+    }),
     // TODO: add roles, cart and orders
   },
 });
