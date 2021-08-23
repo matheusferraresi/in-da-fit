501709603bcdb1110aa733c89ace8efcd2ca0f5a
Matheus Ferraresi <matheusferraresi@live.com>
2021-08-23 13:03:28 +0100

    adding cart open/close functionality

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import CartStyles from './styles/CartStyles';
+import CloseButton from './styles/CloseButton';
 import Supreme from './styles/Supreme';
 import { useUser } from './User';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
+import { useCart } from '../lib/cartState';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -48,13 +50,17 @@ function CartItem({ cartItem }) {
 
 export default function Cart() {
   const currentUser = useUser();
+  const { cartOpen, closeCart } = useCart();
 
   if (!currentUser) return null;
   return (
-    <CartStyles open>
+    <CartStyles open={cartOpen}>
       <header>
         <Supreme>{currentUser.name}'s Cart</Supreme>
       </header>
+      <CloseButton type="button" onClick={closeCart}>
+        &times;
+      </CloseButton>
       <ul>
         {currentUser.cart.map((cartItem) => (
           <CartItem key={cartItem.id} cartItem={cartItem} />
